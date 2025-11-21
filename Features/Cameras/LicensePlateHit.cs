18d51f9f03b54b7a088f60ea4c975dcdfca1c3f8
Astro <astroburgers1@gmail.com>
2025-11-21 16:32:31 -0600

    Added XML docs to public things

--- Features/Cameras/LicensePlateHit.cs
+++ Features/Cameras/LicensePlateHit.cs
@@ -1,6 +1,16 @@
 ﻿namespace GRIDWATCH.Features.Cameras;
 
-public struct LicensePlateHit(
+/// <summary>
+/// Represents a license plate hit detected by the system.
+/// </summary>
+/// <param name="location">The geographical location where the license plate was detected.</param>
+/// <param name="licensePlate">The detected license plate number.</param>
+/// <param name="make">The make of the vehicle.</param>
+/// <param name="model">The model of the vehicle.</param>
+/// <param name="primaryColor">The primary color of the vehicle.</param>
+/// <param name="secondaryColor">The secondary color of the vehicle.</param>
+/// <param name="message">An additional message or note related to the license plate hit.</param>
+public readonly struct LicensePlateHit(
     Vector3 location,
     string licensePlate,
     string make,
@@ -9,12 +19,43 @@ public struct LicensePlateHit(
     string secondaryColor,
     string message)
 {
+    /// <summary>
+    /// Gets the geographical location where the license plate was detected.
+    /// </summary>
     public Vector3 Location { get; } = location;
+
+    /// <summary>
+    /// Gets the detected license plate number.
+    /// </summary>
     public string LicensePlate { get; } = licensePlate;
+
+    /// <summary>
+    /// Gets the timestamp when the license plate hit was recorded.
+    /// </summary>
     public DateTime Timestamp { get; } = DateTime.Now;
+
+    /// <summary>
+    /// Gets the make of the vehicle.
+    /// </summary>
     public string Make { get; } = make;
+
+    /// <summary>
+    /// Gets the model of the vehicle.
+    /// </summary>
     public string Model { get; } = model;
+
+    /// <summary>
+    /// Gets the primary color of the vehicle.
+    /// </summary>
     public string PrimaryColor { get; } = primaryColor;
+
+    /// <summary>
+    /// Gets the secondary color of the vehicle.
+    /// </summary>
     public string SecondaryColor { get; } = secondaryColor;
+
+    /// <summary>
+    /// Gets an additional message or note related to the license plate hit.
+    /// </summary>
     public string Message { get; } = message;
 }
--- Features/Shotspotter/GunfireIncident.cs
+++ Features/Shotspotter/GunfireIncident.cs
@@ -1,10 +1,36 @@
 ﻿namespace GRIDWATCH.Features.Shotspotter;
 
+/// <summary>
+/// Represents a gunfire incident detected by the system.
+/// </summary>
+/// <param name="loc">The location where the gunfire incident occurred.</param>
+/// <param name="shooter">The shooter involved in the gunfire incident.</param>
+/// <param name="weapon">The weapon used in the gunfire incident.</param>
+/// <param name="hit">Indicates whether the gunfire resulted in a confirmed hit (default is false).</param>
 public readonly struct GunfireIncident(Vector3 loc, Ped shooter, string weapon, bool hit = false)
 {
+    /// <summary>
+    /// Gets the location where the gunfire incident occurred.
+    /// </summary>
     public Vector3 Location { get; } = loc;
+
+    /// <summary>
+    /// Gets the shooter involved in the gunfire incident.
+    /// </summary>
     public Ped Shooter { get; } = shooter;
+
+    /// <summary>
+    /// Gets a value indicating whether the gunfire resulted in a confirmed hit.
+    /// </summary>
     public bool ConfirmedHit { get; } = hit;
+
+    /// <summary>
+    /// Gets the weapon used in the gunfire incident.
+    /// </summary>
     public string Weapon { get; } = weapon;
+
+    /// <summary>
+    /// Gets the timestamp when the gunfire incident was recorded.
+    /// </summary>
     public DateTime Timestamp { get; } = DateTime.Now;
 }
